37bcc3df392ce24edab2a2f539d85ca8eb92853d
Aleksey <d_team-lex@bk.ru>
2022-12-20 19:58:23 +0300

    delete folder

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -28,29 +28,7 @@
       </li>
     </ul>
     <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
+
     <h3>Ecosystem</h3>
     <ul>
       <li>
@@ -88,7 +66,7 @@
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
     msg: String,
   },
