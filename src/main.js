427866a525c414a84d12e1d174357f7f7513a80b
Aleksey <d_team-lex@bk.ru>
2023-01-16 18:00:54 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './assets/global.scss';
 
 import { createApp } from 'vue';
-import store from '@/store';
+import store from './store';
 import App from './App.vue';
 
 createApp(App).use(store).mount('#app');
